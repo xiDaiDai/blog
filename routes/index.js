8788fe313452b2b74a1b27ef5382d32eaf1dd909
xishiwei <819520976@qq.com>
2016-06-29 17:22:20 +0800

    add search

--- routes/index.js
+++ routes/index.js
@@ -450,6 +450,25 @@ module.exports = function(app) {
 			});
 	});
 
+
+	app.get('/search', function(req, res) {
+		Post.find({
+			title: req.query.keyword
+		}, function(err, posts) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('/');
+			}
+			res.render('search', {
+				title: "SEARCH:" + req.query.keyword,
+				posts: posts,
+				user: req.session.user,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
+
 	function checkLogin(req, res, next) {
 		if (!req.session.user) {
 			req.flash('error', '未登录!');
